21235268e0d429242d035a43c855d0975a8be68b
Aariz-Haque <haqaariz2008@gmail.com>
2023-09-14 15:24:43 +0530

    Implemented basic IO

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from Intelligence.conversation import getResponse
+from IO.listen import listen
+from IO.speak import speak
 while True:
-    q=input("Enter query")
+    q=listen()
     r=getResponse(q)
     print(r)
+    speak(r)
+    
